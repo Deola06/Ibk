451846ab560af31b02d5cf97177e0b1b6e249d0d
Adeola <165338176+Deola06@users.noreply.github.com>
2025-01-21 05:10:37 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  safelist: [
+    'bg-dark-brown',
+    'text-dark-brown',
+    'bg-brown',
+    'text-brown',
+    'bg-light-cream',
+    'text-light-cream',
+    'bg-main',
+    'dark-bg-main',
+  ],
+
+  darkMode: 'media', // Enables dark mode based on user system preference
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brown: '#481a13',
+        'dark-brown': '#241614',
+        'light-cream': '#ebe6e0',
+        'bg-main': '#646887',
+        'dark-bg-main': '#26273b',
+        'hover-text': '#481a13',
+      },
+      fontFamily: {
+        plain: ['Inter', 'sans-serif'],
+        fun: ['Viaoda Libre', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
